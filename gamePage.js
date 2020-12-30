e7bf4633babf2bb49d5ffdf56d162a265b445dbb
CoderTvisha <71917050+CoderTvisha@users.noreply.github.com>
2020-12-30 17:11:03 +0530

    Add files via upload

---
+++ gamePage.js
@@ -0,0 +1,14 @@
+
+	var player1_score = 0;
+	var player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+player1_name = localStorage.getItem("player1_name");
+
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+player2_name = localStorage.getItem("player2_name");
+
+document.getElementById("player1_scoreBoard").innerHTML = player1_name + ": " + player1_score ;
+document.getElementById("player2_scoreBoard").innerHTML = player2_name + ": " + player2_score ;
+
+
